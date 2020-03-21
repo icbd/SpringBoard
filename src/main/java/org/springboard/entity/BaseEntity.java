dfba5ce398f4a9ffbf0a8bad9da5c59e3451e224
Baodong <wwwicbd@gmail.com>
2020-03-21 12:30:16 +0800

    FEAT -- Add BaseEntity

---
+++ src/main/java/org/springboard/entity/BaseEntity.java
@@ -0,0 +1,38 @@
+package org.springboard.entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@SuperBuilder
+@Data
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+public abstract class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @NotNull
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+}

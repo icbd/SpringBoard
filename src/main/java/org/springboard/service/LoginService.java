7f0a4bd6baae91919aa9ca8f6fcc1e07324bfeaa
Baodong <wwwicbd@gmail.com>
2020-03-22 20:22:30 +0800

    FEAT -- Add LoginService

---
+++ src/main/java/org/springboard/service/LoginService.java
@@ -0,0 +1,33 @@
+package org.springboard.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springboard.entity.AccessToken;
+import org.springboard.entity.User;
+import org.springboard.exception.PermissionErrorException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import javax.persistence.EntityNotFoundException;
+
+@RequiredArgsConstructor
+@Service
+public class LoginService {
+
+    private final UserService userService;
+    private final AccessTokenService accessTokenService;
+    private final PasswordEncoder passwordEncoder;
+
+    public AccessToken loginByPassword(String email, String password) {
+        User user;
+        try {
+            user = userService.getUserByEmail(email);
+            if (!passwordEncoder.matches(password, user.getPasswordDigest())) {
+                throw new EntityNotFoundException();
+            }
+        } catch (EntityNotFoundException e) {
+            throw new PermissionErrorException("Incorrect Email or Password.");
+        }
+
+        return accessTokenService.findOrCreateAccessToken(user);
+    }
+}

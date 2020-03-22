e75877499acd27255444ae9b451f73fdf5232696
Baodong <wwwicbd@gmail.com>
2020-03-22 20:23:20 +0800

    FEAT -- Add methods to AccessTokenService

--- src/main/java/org/springboard/service/AccessTokenService.java
+++ src/main/java/org/springboard/service/AccessTokenService.java
@@ -4,10 +4,12 @@ import lombok.RequiredArgsConstructor;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.springboard.entity.AccessToken;
 import org.springboard.entity.User;
+import org.springboard.exception.PermissionErrorException;
 import org.springboard.repository.AccessTokenRepository;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 @RequiredArgsConstructor
@@ -22,8 +24,31 @@ public class AccessTokenService {
         return accessTokenRepository.getOne(id);
     }
 
+    public AccessToken getAccessToken(String token) {
+        AccessToken accessToken = accessTokenRepository.getByToken(token);
+        if (accessToken == null) {
+            throw new PermissionErrorException("Token is invalid.");
+        } else if (accessToken.getExpiredAt().isBefore(LocalDateTime.now())) {
+            throw new PermissionErrorException("Token has expired.");
+        }
+
+        return accessToken;
+    }
+
+    public List<AccessToken> findAvailableAccessToken(User user) {
+        return accessTokenRepository.findAvailableByUserId(user.getId(), LocalDateTime.now());
+    }
+
     public Optional<AccessToken> findAccessTokenById(Long id) {
         return accessTokenRepository.findById(id);
+    }
+
+    public AccessToken findOrCreateAccessToken(User user) {
+        List<AccessToken> availableAccessTokens = findAvailableAccessToken(user);
+        if (availableAccessTokens.size() > 0) {
+            return availableAccessTokens.get(0);
+        }
+        return createAccessToken(user);
     }
 
     public AccessToken createAccessToken(User user) {
@@ -47,5 +72,9 @@ public class AccessTokenService {
 
     public void destroyAccessToken(Long id) {
         accessTokenRepository.deleteById(id);
+    }
+
+    public void destroyAllAccessTokens(User user) {
+        accessTokenRepository.deleteAllByUser(user);
     }
 }

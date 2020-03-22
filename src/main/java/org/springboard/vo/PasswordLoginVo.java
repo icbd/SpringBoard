06d3bf0ab95ab6c1c9a16fe1123eb2b8d5b1af5c
Baodong <wwwicbd@gmail.com>
2020-03-22 20:20:45 +0800

    Create PasswordLoginVo.java

---
+++ src/main/java/org/springboard/vo/PasswordLoginVo.java
@@ -0,0 +1,18 @@
+package org.springboard.vo;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+@Getter
+@Setter
+public class PasswordLoginVo {
+
+    @Email
+    private String email;
+
+    @NotBlank
+    private String password;
+}

780e9c0b4699ea2477aadea69f1b96d7289f3884
Baodong <wwwicbd@gmail.com>
2020-03-22 21:10:38 +0800

    FIX -- Fix user controller, without uuid

--- src/main/java/org/springboard/controller/api/v1/UserController.java
+++ src/main/java/org/springboard/controller/api/v1/UserController.java
@@ -1,18 +1,17 @@
 package org.springboard.controller.api.v1;
 
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
-import org.springboard.vo.CreateUserVo;
-import org.springboard.vo.UpdateUserVo;
 import org.springboard.dto.UserDto;
 import org.springboard.entity.User;
 import org.springboard.mapper.UserMapper;
 import org.springboard.service.UserService;
+import org.springboard.vo.CreateUserVo;
+import org.springboard.vo.UpdateUserVo;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,23 +19,25 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
+/**
+ * 用户个人中心
+ */
 @RequiredArgsConstructor
 @RestController
-@RequestMapping(BaseController.PREFIX + "/users")
+@RequestMapping(BaseController.PREFIX + "/user")
 public class UserController extends BaseController {
 
     private final UserService userService;
     private final UserMapper userMapper;
 
-    @GetMapping("/{uuid}")
-    public ResponseEntity<UserDto> show(@PathVariable String uuid) {
-        User user = userService.getUserByUuid(uuid);
-        assertOnlyFor(user);
-
-        UserDto userDto = userMapper.toUserDto(user);
+    @ApiOperation("查看个人信息")
+    @GetMapping
+    public ResponseEntity<UserDto> show() {
+        UserDto userDto = userMapper.toUserDto(getCurrentUser());
         return ResponseEntity.ok(userDto);
     }
 
+    @ApiOperation("用户注册")
     @PostMapping
     public ResponseEntity<UserDto> create(@Valid @RequestBody CreateUserVo vo) {
         User user = userService.createUser(vo);
@@ -44,22 +45,12 @@ public class UserController extends BaseController {
         return ResponseEntity.status(HttpStatus.CREATED).body(userDto);
     }
 
-    @PatchMapping("/{uuid}")
-    public ResponseEntity<UserDto> update(@PathVariable String uuid, @RequestBody UpdateUserVo vo) {
-        User user = userService.getUserByUuid(uuid);
-        assertOnlyFor(user);
-
+    @ApiOperation("修改个人信息")
+    @PatchMapping
+    public ResponseEntity<UserDto> update(@RequestBody UpdateUserVo vo) {
+        User user = getCurrentUser();
         userService.updateUser(user, vo);
         UserDto userDto = userMapper.toUserDto(user);
         return ResponseEntity.ok(userDto);
-    }
-
-    @DeleteMapping("/{uuid}")
-    public ResponseEntity<Void> destroy(@PathVariable String uuid) {
-        User user = userService.getUserByUuid(uuid);
-        assertOnlyFor(user);
-
-        userService.destroyUser(user.getId());
-        return ResponseEntity.noContent().build();
     }
 }

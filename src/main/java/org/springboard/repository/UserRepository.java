6f0dba05c8993aa0022d95ffeabaaa7ceb243728
Baodong <wwwicbd@gmail.com>
2020-03-22 20:23:26 +0800

    Update UserRepository.java

--- src/main/java/org/springboard/repository/UserRepository.java
+++ src/main/java/org/springboard/repository/UserRepository.java
@@ -6,4 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface UserRepository extends JpaRepository<User, Long> {
 
     User getByUuid(String uuid);
+
+    User getByEmail(String email);
 }

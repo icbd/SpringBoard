175d2d71afa94857ec472faed13eb9a52497108a
Baodong <wwwicbd@gmail.com>
2020-03-24 22:38:28 +0800

    FEAT -- Add listing and task table sql

--- src/main/resources/db/migration/V20200324190000__Create_listing_table.sql
+++ src/main/resources/db/migration/V20200324190000__Create_listing_table.sql
@@ -1,7 +1,8 @@
-CREATE TABLE IF NOT EXISTS product
+CREATE TABLE IF NOT EXISTS listing
 (
     `id`          bigint       NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `uuid`        varchar(255) NOT NULL UNIQUE,
+    `project_id`  bigint       NOT NULL,
     `creator_id`  bigint       NOT NULL,
     `title`       varchar(255) NOT NULL,
     `description` text,
@@ -11,4 +12,6 @@ CREATE TABLE IF NOT EXISTS product
     `deleted_at`  timestamp             DEFAULT NULL
 );
 
-CREATE INDEX "index_product_on_creator_id" ON product ("creator_id");
+CREATE INDEX "index_listing_on_project_id" ON listing ("project_id");
+
+CREATE INDEX "index_listing_on_creator_id" ON listing ("creator_id");
--- src/main/resources/db/migration/V20200324220000__Create_task_table.sql
+++ src/main/resources/db/migration/V20200324220000__Create_task_table.sql
@@ -1,8 +1,9 @@
-CREATE TABLE IF NOT EXISTS product
+CREATE TABLE IF NOT EXISTS task
 (
     `id`          bigint       NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `uuid`        varchar(255) NOT NULL UNIQUE,
     `creator_id`  bigint       NOT NULL,
+    `parent_id`   bigint,
     `title`       varchar(255) NOT NULL,
     `description` text,
 
@@ -11,4 +12,6 @@ CREATE TABLE IF NOT EXISTS product
     `deleted_at`  timestamp             DEFAULT NULL
 );
 
-CREATE INDEX "index_product_on_creator_id" ON product ("creator_id");
+CREATE INDEX "index_task_on_creator_id" ON task ("creator_id");
+
+CREATE INDEX "index_task_on_parent_id" ON task ("parent_id");

192e42966551d4f8c2c131316efb4e68c9c7ebe1
Baodong <wwwicbd@gmail.com>
2020-03-29 13:42:54 +0800

    Create Task.java

---
+++ src/main/java/org/springboard/entity/Task.java
@@ -0,0 +1,52 @@
+package org.springboard.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import javax.validation.constraints.NotBlank;
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+@Entity
+@EqualsAndHashCode(callSuper = true)
+public class Task extends BaseEntity {
+
+    @NotBlank
+    private String uuid;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JsonIgnore
+    private User creator;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JsonIgnore
+    private Listing listing;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_id", referencedColumnName = "id")
+    @JsonIgnore
+    private Task parentTask;
+
+    @NotBlank
+    private String title;
+
+    private String description;
+
+    @ApiModelProperty("完成时间 (Task未完成时此值为null)")
+    private LocalDateTime completedAt;
+
+    private LocalDateTime deletedAt;
+}

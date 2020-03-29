69a1f65dec0f6bc10d14ba159b087bb1ba551cb7
Baodong <wwwicbd@gmail.com>
2020-03-29 13:25:13 +0800

    DOC -- Foc FillUuidListener

--- src/main/java/org/springboard/listener/FillUuidListener.java
+++ src/main/java/org/springboard/listener/FillUuidListener.java
@@ -7,6 +7,9 @@ import org.springframework.stereotype.Component;
 import javax.persistence.PrePersist;
 import java.util.UUID;
 
+/**
+ * 如果 uuid 可写(存在且可写), 并且 uuid 尚未赋值, 则自动填充 uuid
+ */
 @Component
 public class FillUuidListener {
 
@@ -15,8 +18,9 @@ public class FillUuidListener {
     @PrePersist
     protected void fillUuidBeforeCreate(Object entity) throws Exception {
         if (PropertyUtils.isWriteable(entity, UUID_FIELD) &&
-                PropertyUtils.getProperty(entity, "uuid") == null) {
-            PropertyUtils.setProperty(entity, "uuid", UUID.randomUUID().toString());
+                PropertyUtils.getProperty(entity, UUID_FIELD) == null) {
+
+            PropertyUtils.setProperty(entity, UUID_FIELD, UUID.randomUUID().toString());
         }
     }
 }

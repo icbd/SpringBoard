a28cf954d7dbe635654df7b2da49fa31a2b8d346
Baodong <wwwicbd@gmail.com>
2020-04-01 15:17:36 +0800

    REFCT -- Change uuid to char(36)

--- src/main/resources/db/migration/V20200321100000__Create_user_table.sql
+++ src/main/resources/db/migration/V20200321100000__Create_user_table.sql
@@ -1,15 +1,15 @@
 CREATE TABLE IF NOT EXISTS user
 (
     `id`              bigint       NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `uuid`            varchar(255) NOT NULL UNIQUE,
-    `enabled`         tinyint      NOT NULL DEFAULT 0,
+    `uuid`            char(36)     NOT NULL UNIQUE DEFAULT (uuid()),
+    `enabled`         tinyint      NOT NULL        DEFAULT 0,
     `email`           varchar(255) NOT NULL UNIQUE,
-    `name`            varchar(255)          DEFAULT NULL,
+    `name`            varchar(255)                 DEFAULT NULL,
     `password_digest` varchar(255) NOT NULL,
 
-    `created_at`      timestamp    NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    `updated_at`      timestamp    NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    `deleted_at`      timestamp             DEFAULT NULL
+    `created_at`      timestamp    NOT NULL        DEFAULT CURRENT_TIMESTAMP,
+    `updated_at`      timestamp    NOT NULL        DEFAULT CURRENT_TIMESTAMP,
+    `deleted_at`      timestamp                    DEFAULT NULL
 );
 
 CREATE INDEX index_user_on_deleted_at ON user (deleted_at);

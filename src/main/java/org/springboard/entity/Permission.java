6db3cc5e97e1e49b1121e3c705301217eceb6a9e
Baodong <wwwicbd@gmail.com>
2020-04-12 21:34:44 +0800

    Update Permission.java

--- src/main/java/org/springboard/entity/Permission.java
+++ src/main/java/org/springboard/entity/Permission.java
@@ -1,5 +1,6 @@
 package org.springboard.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -8,7 +9,10 @@ import lombok.NoArgsConstructor;
 import org.springboard.constant.PermissionEnum;
 
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.List;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -17,6 +21,11 @@ import javax.validation.constraints.NotNull;
 @Entity
 @EqualsAndHashCode(callSuper = true)
 public class Permission extends BaseEntity {
+
+    @Builder.Default
+    @JsonIgnore
+    @OneToMany(mappedBy = "permission")
+    private List<RoleAndPermission> roleAndPermissions = new ArrayList<>();
 
     @NotNull
     private String sourceType;

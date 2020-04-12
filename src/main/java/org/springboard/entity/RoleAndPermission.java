52852fbbf49ed1a0ebd7b87379959d253327fac3
Baodong <wwwicbd@gmail.com>
2020-04-12 21:34:51 +0800

    Create RoleAndPermission.java

---
+++ src/main/java/org/springboard/entity/RoleAndPermission.java
@@ -0,0 +1,31 @@
+package org.springboard.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+@Entity
+@EqualsAndHashCode(callSuper = true)
+public class RoleAndPermission extends BaseEntity {
+
+    @ManyToOne
+    @JoinColumn(name = "role_id")
+    @JsonIgnore
+    private Role role;
+
+    @ManyToOne
+    @JoinColumn(name = "permission_id")
+    @JsonIgnore
+    private Permission permission;
+}

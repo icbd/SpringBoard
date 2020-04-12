bb7bd4eb977249625973d51279bcc506aecdf9f7
Baodong <wwwicbd@gmail.com>
2020-04-12 21:34:48 +0800

    Create Role.java

---
+++ src/main/java/org/springboard/entity/Role.java
@@ -0,0 +1,45 @@
+package org.springboard.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.validation.constraints.NotBlank;
+import java.util.ArrayList;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+@Entity
+@EqualsAndHashCode(callSuper = true)
+public class Role extends BaseEntity {
+
+    @OneToOne
+    @JsonIgnore
+    private User creator;
+
+    @ManyToOne
+    @JoinColumn(name = "parent_id", referencedColumnName = "id")
+    @JsonIgnore
+    private Role parentRole;
+
+    @Builder.Default
+    @JsonIgnore
+    @OneToMany(mappedBy = "role")
+    private List<RoleAndPermission> roleAndPermissions = new ArrayList<>();
+
+    @NotBlank
+    private String title;
+
+    private String description;
+}

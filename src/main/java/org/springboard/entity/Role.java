4acb94639f97d0c422ecc2f2c3bc6ff4224ba3dd
Baodong <wwwicbd@gmail.com>
2020-04-13 09:35:43 +0800

    Update Role.java

--- src/main/java/org/springboard/entity/Role.java
+++ src/main/java/org/springboard/entity/Role.java
@@ -6,13 +6,16 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Type;
 
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -21,6 +24,10 @@ import java.util.List;
 @Entity
 @EqualsAndHashCode(callSuper = true)
 public class Role extends BaseEntity {
+
+    @NotNull
+    @Type(type = "uuid-char")
+    private UUID uuid;
 
     @OneToOne
     @JsonIgnore

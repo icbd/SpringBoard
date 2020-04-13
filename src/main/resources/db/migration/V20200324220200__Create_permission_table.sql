86f6fa3d108cf87e6dd9f712602c6380e83f4b7b
Baodong <wwwicbd@gmail.com>
2020-04-13 09:35:59 +0800

    Update V20200324220200__Create_permission_table.sql

--- src/main/resources/db/migration/V20200324220200__Create_permission_table.sql
+++ src/main/resources/db/migration/V20200324220200__Create_permission_table.sql
@@ -1,14 +1,15 @@
 CREATE TABLE IF NOT EXISTS permission
 (
     id          bigint       NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    uuid        char(36)     NOT NULL UNIQUE DEFAULT (uuid()),
     source_type varchar(255) NOT NULL,
     source_id   bigint       NOT NULL,
     code        tinyint      NOT NULL,
     description text,
 
-    created_at  timestamp    NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated_at  timestamp    NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    deleted_at  timestamp             DEFAULT NULL
+    created_at  timestamp    NOT NULL        DEFAULT CURRENT_TIMESTAMP,
+    updated_at  timestamp    NOT NULL        DEFAULT CURRENT_TIMESTAMP,
+    deleted_at  timestamp                    DEFAULT NULL
 );
 
 CREATE INDEX index_permission_on_deleted_at ON permission (deleted_at);

c3d29f4e865ad50dd60356e15d57ada7b8d270f7
Baodong <wwwicbd@gmail.com>
2020-04-13 09:19:32 +0800

    Update RoleAndPermissionRepository.java

--- src/main/java/org/springboard/repository/RoleAndPermissionRepository.java
+++ src/main/java/org/springboard/repository/RoleAndPermissionRepository.java
@@ -5,12 +5,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import javax.validation.constraints.NotNull;
-import java.util.List;
 
 public interface RoleAndPermissionRepository extends JpaRepository<RoleAndPermission, Long> {
 
     @Query("select rp from RoleAndPermission rp where" +
-            " rp.role.id = :roleId and rp.permission.id = :permissionId and rp.deletedAt is null" +
-            " order by rp.id")
-    List<RoleAndPermission> findByRoleAndAndPermission(@NotNull Long roleId, @NotNull Long permissionId);
+            " rp.role.id = :roleId and rp.permission.id = :permissionId")
+    RoleAndPermission findByRoleAndAndPermission(@NotNull Long roleId, @NotNull Long permissionId);
 }

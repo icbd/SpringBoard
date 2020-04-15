3e62b3d7bd311300a3bc94f1449a6cfb0fe06901
Baodong <wwwicbd@gmail.com>
2020-04-15 22:29:54 +0800

    Update Permission.java

--- src/main/java/org/springboard/entity/Permission.java
+++ src/main/java/org/springboard/entity/Permission.java
@@ -10,7 +10,7 @@ import org.hibernate.annotations.Type;
 import org.springboard.constant.PermissionEnum;
 
 import javax.persistence.Entity;
-import javax.persistence.OneToMany;
+import javax.persistence.ManyToMany;
 import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.List;
@@ -29,9 +29,9 @@ public class Permission extends BaseEntity {
     private UUID uuid;
 
     @Builder.Default
+    @ManyToMany(mappedBy = "permissions")
     @JsonIgnore
-    @OneToMany(mappedBy = "permission")
-    private List<RoleAndPermission> roleAndPermissions = new ArrayList<>();
+    private List<Role> roles = new ArrayList<>();
 
     @NotNull
     private String sourceType;

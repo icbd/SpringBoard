151e27d8c5f0671c62d3362740821e0580e1b0bc
Baodong <wwwicbd@gmail.com>
2020-04-15 22:54:59 +0800

    Update RoleAndPermissionServiceTest.java

--- src/test/java/org/springboard/service/RoleAndPermissionServiceTest.java
+++ src/test/java/org/springboard/service/RoleAndPermissionServiceTest.java
@@ -3,40 +3,30 @@ package org.springboard.service;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import org.springboard.constant.PermissionEnum;
 import org.springboard.entity.Permission;
-import org.springboard.entity.Product;
 import org.springboard.entity.Role;
-import org.springboard.entity.RoleAndPermission;
 import org.springboard.entity.User;
-import org.springboard.repository.PermissionRepository;
 import org.springboard.repository.RoleAndPermissionRepository;
 import org.springboard.repository.RoleRepository;
 import org.springboard.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
-import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.springboard.util.ListHelper.sample;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 
 @DataJpaTest
 @Nested
 class RoleAndPermissionServiceTest extends ServiceTestBase {
 
     @Autowired
-    private UserRepository userRepository;
-
-    @Autowired
     private RoleRepository roleRepository;
 
     @Autowired
-    private PermissionRepository permissionRepository;
+    private UserRepository userRepository;
 
     @Autowired
     private RoleAndPermissionRepository roleAndPermissionRepository;
@@ -44,67 +34,36 @@ class RoleAndPermissionServiceTest extends ServiceTestBase {
     @Autowired
     private RoleAndPermissionService roleAndPermissionService;
 
-    private User creator;
-    private List<RoleAndPermission> roleAndPermissions = new ArrayList<>();
-    private RoleAndPermission aCase;
+    private Role role;
+    private List<Permission> permissions;
 
     @BeforeEach
     void setUp() {
-        creator = userRepository.save(buildUser());
-        List<Permission> permissions = permissionRepository.saveAll(buildPermissions());
-        Role role = roleRepository.save(buildRole(creator));
-
-        roleAndPermissions = permissions.stream()
-                                        .map(permission -> {
-                                            RoleAndPermission roleAndPermission =
-                                                    RoleAndPermission.builder().role(role).permission(permission)
-                                                                     .build();
-                                            return roleAndPermissionRepository.save(roleAndPermission);
-                                        })
-                                        .collect(Collectors.toList());
-        aCase = sample(roleAndPermissions);
+        User user = userRepository.save(buildUser());
+        role = buildRole(user);
+        permissions = buildPermissions();
+        role.setPermissions(permissions);
+        roleRepository.save(role);
     }
 
     @Test
-    void getRoleAndPermissionByIdTest() {
-        RoleAndPermission roleAndPermissionById = roleAndPermissionService.getRoleAndPermissionById(aCase.getId());
-        assertEquals(aCase.getRole().getId(), roleAndPermissionById.getRole().getId());
+    void findPermissionsByRoleIdTest() {
+        List<Permission> permissionsByRoleId = roleAndPermissionService.findPermissionsByRoleId(role.getId());
+        assertEquals(permissions.size(), permissionsByRoleId.size());
+        assertTrue(permissionsByRoleId.containsAll(permissions));
     }
 
     @Test
-    void findByRoleAndAndPermissionTest() {
-        RoleAndPermission byRoleAndAndPermission = roleAndPermissionService
-                .findByRoleAndAndPermission(aCase.getRole(), aCase.getPermission());
-
-        assertEquals(aCase.getId(), byRoleAndAndPermission.getId());
+    void findRolesByPermissionIdTest() {
+        List<Role> roles = roleAndPermissionService.findRolesByPermissionId(permissions.get(0).getId());
+        assertEquals(1, roles.size());
+        assertTrue(roles.contains(role));
     }
 
     @Test
-    void bindBetweenTest() {
-        long count = roleAndPermissionRepository.count();
-        Role role = aCase.getRole();
-        Permission permission = permissionRepository.save(
-                buildPermission(Product.class.getSimpleName(), new Random().nextLong(), PermissionEnum.ADMIN));
-        roleAndPermissionService.bindBetween(role, permission);
-
-        assertEquals(count + 1, roleAndPermissionRepository.count());
-    }
-
-    @Test
-    void unbindBetweenTest() {
-        long count = roleAndPermissionRepository.count();
-        Role role = aCase.getRole();
-        Permission permission = aCase.getPermission();
-        roleAndPermissionService.unbindBetween(role, permission);
-
-        assertEquals(count - 1, roleAndPermissionRepository.count());
-        assertNull(roleAndPermissionService.findByRoleAndAndPermission(role, permission));
-    }
-
-    @Test
-    void destroyRoleAndPermissionTest() {
-        long count = roleAndPermissionRepository.count();
-        roleAndPermissionService.destroyRoleAndPermission(aCase.getId());
+    void deleteByRoleAndPermissionTest() {
+        Long count = roleAndPermissionRepository.count();
+        roleAndPermissionService.deleteByRoleAndPermission(role, permissions.get(0));
         assertEquals(count - 1, roleAndPermissionRepository.count());
     }
 }

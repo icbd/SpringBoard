0eaecbc42093f6f6016dacc29144dea4dec08fb3
Baodong <wwwicbd@gmail.com>
2020-04-15 22:54:05 +0800

    Update RoleAndPermissionRepository.java

--- src/main/java/org/springboard/repository/RoleAndPermissionRepository.java
+++ src/main/java/org/springboard/repository/RoleAndPermissionRepository.java
@@ -16,4 +16,6 @@ public interface RoleAndPermissionRepository extends JpaRepository<RoleAndPermis
 
     @Query("select rp.permission from RoleAndPermission rp where rp.role.id = :roleId")
     List<Permission> findPermissionsByRoleId(@NotNull Long roleId);
+
+    void deleteByRoleAndPermission(Role role, Permission permission);
 }

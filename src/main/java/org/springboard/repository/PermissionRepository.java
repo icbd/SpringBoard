1dd6783ea77c18e7028ae0d61173859ef49530d4
Baodong <wwwicbd@gmail.com>
2020-04-18 17:32:32 +0800

    Update PermissionRepository.java

--- src/main/java/org/springboard/repository/PermissionRepository.java
+++ src/main/java/org/springboard/repository/PermissionRepository.java
@@ -1,5 +1,6 @@
 package org.springboard.repository;
 
+import org.springboard.constant.PermissionEnum;
 import org.springboard.entity.Permission;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -10,4 +11,6 @@ public interface PermissionRepository extends JpaRepository<Permission, Long> {
 
     @Query("select pms from Permission pms where pms.id in :permissionIds")
     List<Permission> findByIds(List<Long> permissionIds);
+
+    List<Permission> findBySourceTypeAndSourceIdAndCodeIsIn(String sourceType, Long sourceId, List<PermissionEnum> codes);
 }

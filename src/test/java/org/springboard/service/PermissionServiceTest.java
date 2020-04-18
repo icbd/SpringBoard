89ef500ce59979fb41c20d44f99d73cdd18bcca1
Baodong <wwwicbd@gmail.com>
2020-04-18 17:33:27 +0800

    Update PermissionServiceTest.java

--- src/test/java/org/springboard/service/PermissionServiceTest.java
+++ src/test/java/org/springboard/service/PermissionServiceTest.java
@@ -3,14 +3,17 @@ package org.springboard.service;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.springboard.constant.PermissionEnum;
 import org.springboard.entity.BaseEntity;
 import org.springboard.entity.Permission;
+import org.springboard.entity.Product;
 import org.springboard.repository.PermissionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -54,6 +57,46 @@ class PermissionServiceTest extends ServiceTestBase {
         List<Permission> permissionsByIds = permissionService.findPermissionsByIds(caseIds);
         assertEquals(caseIds.size(), permissionsByIds.size());
         assertTrue(caseIds.containsAll(permissionsByIds.stream().map(BaseEntity::getId).collect(Collectors.toList())));
+    }
+
+    @Nested
+    class findOrCreateBytest {
+        private String sourceType = Product.class.getSimpleName();
+        private Long sourceId = Math.abs(new Random().nextLong());
+        private List<PermissionEnum> codes = List.of(PermissionEnum.READ, PermissionEnum.EDIT);
+        private List<Permission> existedPermissions;
+
+        @BeforeEach
+        void setUp() {
+            existedPermissions = codes.stream()
+                                      .map(code -> Permission.builder()
+                                                             .sourceType(sourceType)
+                                                             .sourceId(sourceId)
+                                                             .code(code).build())
+                                      .collect(Collectors.toList());
+            permissionRepository.saveAll(existedPermissions);
+        }
+
+        @Test
+        void findExistedTest() {
+            List<Permission> permissions = permissionService.findOrCreateBy(sourceType, sourceId, codes);
+
+            List<PermissionEnum> codesFound = permissions.stream().map(Permission::getCode)
+                                                         .collect(Collectors.toList());
+            assertEquals(codes.size(), codesFound.size());
+            assertTrue(codes.containsAll(codesFound));
+        }
+
+        @Test
+        void createNotExistedTest() {
+            List<PermissionEnum> codes = List.of(PermissionEnum.EDIT, PermissionEnum.ADMIN);
+            List<Permission> permissions = permissionService.findOrCreateBy(sourceType, sourceId, codes);
+
+            List<PermissionEnum> codesFound = permissions.stream().map(Permission::getCode)
+                                                         .collect(Collectors.toList());
+            assertEquals(codes.size(), codesFound.size());
+            assertTrue(codes.containsAll(codesFound));
+        }
     }
 
     @Test
